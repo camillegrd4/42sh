27cf391a39841677927920c50baff0fb98d8691a
Alex Wild <alex.wild@epitech.eu>
2020-05-18 15:18:33 +0200

    adding modification of cmd parsing and remove useless file

--- src/separator/parse_cmd.c
+++ src/separator/parse_cmd.c
@@ -7,11 +7,11 @@
 
 #include "my.h"
 
-int is_not_separator(char current, char plus_one)
+int is_not_separator(char current, char plus_one, char minus_one)
 {
     if (current == ';' ||
-        (current == '|' && plus_one == '|') ||
-        (current == '&' && plus_one == '&'))
+        (current == '|' && (plus_one == '|' || minus_one == '|')) ||
+        (current == '&' && (plus_one == '&' || minus_one == '&')))
         return 0;
     return 1;
 }
@@ -44,9 +44,9 @@ int is_end(char c)
     const char str[2] = "\0\n";
     int i = 0;
 
-    while (i != sizeof(str)) { 
+    while (i != sizeof(str)) {
         if (c == str[i])
-            return 1; 
+            return 1;
         i++;
     }
     return 0;
@@ -57,7 +57,8 @@ char *return_one_cmd(char *line, int *i)
     char *new = calloc(sizeof(char) * 15, sizeof(char));
     int j = 0;
 
-    while (is_not_separator(line[*i], line[*i + 1]) && is_end(line[*i]) != 1) {
+    while (is_not_separator(line[*i], line[*i + 1], line[*i - 1]) &&
+            is_end(line[*i]) != 1) {
         new[j] = line[*i];
         j++;
         (*i)++;
