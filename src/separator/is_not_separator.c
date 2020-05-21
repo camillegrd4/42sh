a1ac4f6032e8050302ce73644a2efcf8195beef0
Grandjean <camille.grandjean@epitech.eu>
2020-05-21 15:53:14 +0200

    \v\f add and functional

--- src/separator/is_not_separator.c
+++ src/separator/is_not_separator.c
@@ -7,12 +7,29 @@
 
 #include "my.h"
 
-int is_not_separator(char current, char plus_one, char minus_one)
+int is_not_next(char *line, int i)
 {
-    if (current == ';' ||
-        (current == '|' && (plus_one == '|' || minus_one == '|')) ||
-        (current == '&' && (plus_one == '&' || minus_one == '&')))
+    if (i == 0 && line[i] == '|' && line[i + 1] == '|')
         return 0;
+    if (is_end(line[i]) && line[i] == '|' && line[i - 1] == '|')
+        return 0;
+    else if (line[i] == '|' && line[i + 1] == '|' && line[i - 1] == '|')
+        return 0;
+    return 1;
+}
+
+int is_not_separator(char *line, int i)
+{
+    if (line[i] == ';')
+        return 0;
+    if (i == 0 && line[i] == '&' && line[i + 1] == '&')
+        return 0;
+    if (is_end(line[i]) && line[i] == '&' && line[i - 1] == '&')
+        return 0;
+    else if (line[i] == '&' && line[i + 1] == '&' && line[i - 1] == '&')
+        return 0;
+    else
+        return is_not_next(line, i);
     return 1;
 }
 
